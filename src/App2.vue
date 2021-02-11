8c0da28406acf7d4898409a5628dd27f2b051cee
Yannick Biheul <yannickbiheul@outlook.fr>
2021-02-11 18:06:03 +0100

    Affichage articles

---
+++ src/App2.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <h1>Hello World !!!!</h1>
+    <h2>{{ message }}</h2>
+</div>
+  
+</template>
+
+<script>
+
+export default {
+    data: () => {
+        return {
+            message: "Hello from data return"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Articles.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ titre }}</h1>
+    <div v-for="unArticle in listeArticles" v-bind:key="unArticle">
+      {{ unArticle }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      titre: "Articles",
+      listeArticles: ['produit1', 'produit2', 'produit3']
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
